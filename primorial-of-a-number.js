2e30333fe7900c0e4c76bf8dd8b8776b7998ce56
ghildreth <gabrielantonhildreth@gmail.com>
2018-07-18 23:26:14 -0700

    add yellow fade transition and isPrime function

--- primorial-of-a-number.js
+++ primorial-of-a-number.js
@@ -28,3 +28,26 @@ function numPrimorial(number){
 
     return primArray.slice(0, number).reduce(reducer);
   }
+
+  // Number one answer
+
+//   function isPrime(num) {
+//     for(var i = 2; i < num; i++){
+//       if(num % i === 0){
+//         return false
+//       }
+//     }
+//     return true
+//   }
+  
+//   function numPrimorial(n){
+//     let arr = []
+//     let i = 2
+//     while(arr.length < n){
+//       if(isPrime(i)){
+//         arr.push(i)
+//       }
+//      i++
+//     }
+//     return arr.reduce((a,b) => a * b)
+//   }
